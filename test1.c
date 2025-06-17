d3ab8755b164e634da8ea996b22a07d7ffc1ae36
Antoine <125296602+PoirierTheGreat0440E@users.noreply.github.com>
2025-06-17 14:29:59 +0200

    Vertex

--- test1.c
+++ test1.c
@@ -5,7 +5,34 @@
 // AIE AIE AIE
 
 #define TAILLE_CARTE 10
-enum Directions { Nord=1 , Sud , Ouest , Est };
+enum Directions { Non=-1 , Nord=1 , Sud , Ouest , Est };
+
+typedef struct {
+	int pos_x;
+	int pos_y;
+	int derniere_direction;
+	struct t_vertex* vertex_suivant;	
+} t_vertex;
+
+t_vertex creer_vertex( int x_init , int y_init , int dir_init , t_vertex* vertex_parent ){
+	t_vertex nouveau_vertex = { x_init , y_init , dir_init , NULL };
+	if ( vertex_parent != NULL ){
+		vertex_parent->vertex_suivant = &nouveau_vertex;
+	}
+	return nouveau_vertex;
+}
+
+void afficher_vertex(t_vertex vertex_cible){
+	printf("[ %d , %d , %d , %x ] ...\n",vertex_cible.pos_x,vertex_cible.pos_y,vertex_cible.derniere_direction,vertex_cible.vertex_suivant);
+}
+
+void afficher_vertex_cascade( t_vertex* vertex_cible ){
+	t_vertex* tete = vertex_cible;
+	while ( tete != NULL ){
+		afficher_vertex(*tete);
+		tete = tete->vertex_suivant;
+	}	
+}
 
 void creer_chemin( int x_init , int y_init , int direction , int pas ,int carte[][TAILLE_CARTE] ){
 	int x_pos = x_init;
@@ -81,13 +108,21 @@ int main(int argc , char** argv){
 
 	int carte_jeu[TAILLE_CARTE][TAILLE_CARTE];
 
-	initialiser_carte(carte_jeu);
+	//initialiser_carte(carte_jeu);
 
-	afficher_carte(carte_jeu);
+	//creer_chemin(3,3,1,10,carte_jeu);
 
-	creer_chemin(3,3,1,10,carte_jeu);
+	t_vertex vertex1 = creer_vertex(3,3,-1,NULL);
 
-	afficher_carte(carte_jeu);
+	t_vertex vertex2 = creer_vertex(4,4,-1,&vertex1);
+	
+	t_vertex vertex3 = creer_vertex(4,4,-1,&vertex2);
+
+	afficher_vertex(vertex3);
+
+	printf("--- TEST CASCADE ---\n");
+
+	afficher_vertex_cascade(&vertex1);
 
 	return 0;
 }
